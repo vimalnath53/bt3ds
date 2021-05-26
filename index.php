1a2003f2c2502916808d933e2c640e70711c13e6
vimalnath53 <vimalnath53@gmail.com>
2021-05-26 10:06:15 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 
 <?php
-// echo 'dsa';exit;
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
