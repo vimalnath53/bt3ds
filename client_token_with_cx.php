6105c046a9e6e24ae1dfc4676327f79d764bd2f5
vimalnath53 <vimalnath53@gmail.com>
2021-10-09 00:39:06 +1100

    Create client_token_with_cx.php

---
+++ client_token_with_cx.php
@@ -0,0 +1,28 @@
+
+<?php
+// echo 'dsa';exit;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require("../vendor/braintree/braintree_php/lib/Braintree.php");
+require("../vendor/braintree/braintree_php/lib/autoload.php");
+
+
+$gateway = new Braintree\Gateway([
+  'environment' => 'sandbox',
+  'merchantId' => 'zynjg7c9rd5c95z2',
+  'publicKey' => 'y44n7myzmhg8x2vz',
+  'privateKey' => 'e67402552c930712192d5a656f0c29dd'
+]);
+
+$clientToken = $gateway->clientToken()->generate([
+  "customerId" => 'Y3VzdG9tZXJfMjI5NTc5OTY4' 
+]);
+$result = $gateway->paymentMethodNonce()->create('94846nr');
+
+
+$data = array();
+$data['token'] = $clientToken;
+$data['pmt'] = $result->paymentMethodNonce->nonce;
+echo json_encode($data);
