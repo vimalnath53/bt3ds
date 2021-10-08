c66bb092ff591d5af03b3ad1db45493a590616de
vimalnath53 <vimalnath53@gmail.com>
2021-10-09 00:51:43 +1100

    Update client_token_with_cx.php

--- client_token_with_cx.php
+++ client_token_with_cx.php
@@ -1,6 +1,12 @@
 
 <?php
-// echo 'dsa';exit;
+<?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
